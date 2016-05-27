ed7776f055ab0fda2cd4865828a9f0c138d7b915
雷日锦 <362606856@qq.com>
2016-05-27 20:14:51 +0800

    online template

--- temp/compiled/themes/admin/template/online.html.php
+++ temp/compiled/themes/admin/template/online.html.php
@@ -6,7 +6,7 @@
 	<a class="item" href="<?php echo APPADMIN; ?>?m=template">本地模板</a>
     <a  class="item active" href="<?php echo APPADMIN; ?>?m=template&a=online">在线模板</a>
 </div>
-<iframe src="http://skyshop.skymvc.com/index.php?m=moban&product=skyCom" style="width:100%; height:600px; border:0px;"></iframe>
+<iframe src="http://www.deitui.com/index.php?m=moban&product=skyCom" style="width:100%; height:600px; border:0px;"></iframe>
  
 <?php echo $this->fetch('footer.html'); ?>
 </body>
